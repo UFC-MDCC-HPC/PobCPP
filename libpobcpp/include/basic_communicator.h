fd4dd888bc51f0fbf5cec6e44a053a1a0f4aa938
Eduardo Gurgel <edgurgel@gmail.com>
2010-05-25 19:16:41 +0000

    * basic_communicator.h,.cpp : gather function added.

--- libpobcpp/include/basic_communicator.h
+++ libpobcpp/include/basic_communicator.h
@@ -21,6 +21,7 @@ public:
 
 // Point-to-Point
 public:
+	//FIXME implement array send and receive
 	template<typename Type, typename Data>
 	void send(const Data& data, const int tag, unsigned int _enumi = 0);
 
@@ -30,16 +31,38 @@ public:
 // Collective
 	template<typename Type, typename Data>
 	void broadcast(Data& value, unsigned int _enumi = 0);
+	template<typename Type, typename Data>
+	void broadcast(Data* values, int n, unsigned int _enumi = 0);
+
+	template<typename Type, typename Data>
+	void gather(const Data& in_value, Data* out_values, unsigned int _enumi = 0);
 
 private:
+	// Implementations
 	void send(const Unit_Type& _unit_type, const int& _data, const int _tag);
+	void send(const Unit_Type& _unit_type, const float& _data, const int _tag);
+	void send(const Unit_Type& _unit_type, const double& _data, const int _tag);
 	void send(const Unit_Type& _unit_type, const std::string& _data, const int _tag);
 
 	int receive(const Unit_Type& _unit_type, const int& _data_type, const int _tag);
+	float receive(const Unit_Type& _unit_type, const float& _data_type, const int _tag);
+	double receive(const Unit_Type& _unit_type, const double& _data_type, const int _tag);
 	std::string receive(const Unit_Type& _unit_type, const std::string& _data_type, const int _tag);
 
-	void broadcast(const Unit_Type& _unit_type, int& value, unsigned int _enumi = 0);
-	void broadcast(const Unit_Type& _unit_type, std::string& value, unsigned int _enumi = 0);
+	void broadcast(const Unit_Type& _unit_type, int& value);
+	void broadcast(const Unit_Type& _unit_type, float& value);
+	void broadcast(const Unit_Type& _unit_type, double& value);
+	void broadcast(const Unit_Type& _unit_type, std::string& value);
+
+	void broadcast(const Unit_Type& _unit_type, int* values, int n);
+	void broadcast(const Unit_Type& _unit_type, float* values, int n);
+	void broadcast(const Unit_Type& _unit_type, double* values, int n);
+	void broadcast(const Unit_Type& _unit_type, std::string* value, int n);
+
+	void gather(const Unit_Type& _unit_type, const int& in_value, int* out_values);
+	void gather(const Unit_Type& _unit_type, const float& in_value, float* out_values);
+	void gather(const Unit_Type& _unit_type, const double& in_value, double* out_values);
+	void gather(const Unit_Type& _unit_type, const std::string& in_value, std::string* out_values);
 private:
 
 	Environment* env;
@@ -48,10 +71,10 @@ private:
 
 template<typename Type, typename Data>
 void Basic_Communicator::send(const Data& _data, const int _tag, unsigned int _enumi) {
-		Type* un;
-		Unit_Type unit_type(un);
-		unit_type.set_enums(std::make_pair(_enumi,0));
-		send(unit_type, _data, _tag);
+	Type* un;
+	Unit_Type unit_type(un);
+	unit_type.set_enums(std::make_pair(_enumi,0));
+	send(unit_type, _data, _tag);
 }
 
 template<typename Type, typename Data>
@@ -62,12 +85,29 @@ Data Basic_Communicator::receive(const int _tag, unsigned int _enumi) {
 	unit_type.set_enums(std::make_pair(_enumi,0));
 	return receive(unit_type, data, _tag);
 }
+
 template<typename Type, typename Data>
 void Basic_Communicator::broadcast(Data& value, unsigned int _enumi) {
 	Type* un;
 	Unit_Type unit_type(un);	
 	unit_type.set_enums(std::make_pair(_enumi,0));
 	return broadcast(unit_type, value);
+}
+
+template<typename Type, typename Data>
+void Basic_Communicator::broadcast(Data* value, int n, unsigned int _enumi) {
+	Type* un;
+	Unit_Type unit_type(un);	
+	unit_type.set_enums(std::make_pair(_enumi,0));
+	return broadcast(unit_type, value, n);
+}
+
+template<typename Type, typename Data>
+void Basic_Communicator::gather(const Data& in_value, Data* out_values, unsigned int _enumi) {
+	Type* un;
+	Unit_Type unit_type(un);	
+	unit_type.set_enums(std::make_pair(_enumi,0));
+	return broadcast(unit_type, in_value, out_values);
 }
 
 }
--- libpobcpp/src/basic_communicator.cpp
+++ libpobcpp/src/basic_communicator.cpp
@@ -3,6 +3,67 @@
 #include <boost/mpi/collectives.hpp>
 #include <iostream>
 
+
+#define Basic_Communicator_send_impl(datatype) \
+void Basic_Communicator::send(const Unit_Type& _unit_type, const datatype& _data, const int _tag) { \
+	boost::mpi::communicator world(comm, boost::mpi::comm_attach); \
+	if(env && world) { \
+		if(env->isComplete()) { \
+			unsigned int unit_rank = env->get_rank(_unit_type); \
+			world.send(unit_rank, _tag, _data);	\
+			std::cout << "Sending " << _data << " to "<< _unit_type << " - " << unit_rank << std::endl; \
+		} \
+	} \
+} 
+
+#define Basic_Communicator_receive_impl(datatype) \
+datatype Basic_Communicator::receive(const Unit_Type& _unit_type, const datatype& _data_type, const int _tag) { \
+	boost::mpi::communicator world(comm, boost::mpi::comm_attach); \
+	datatype data; \
+	if(env && world) { \
+		if(env->isComplete()) { \
+			unsigned int unit_rank = env->get_rank(_unit_type); \
+			world.recv(unit_rank , _tag, data); \
+			std::cout << "Received " << data << " from "<< _unit_type << " - " << unit_rank << std::endl; \
+		} \
+	} \
+	return data; \
+} 
+
+#define Basic_Communicator_broadcast_impl(datatype) \
+void Basic_Communicator::broadcast(const Unit_Type& _unit_type, datatype& value) { \
+	boost::mpi::communicator world(comm, boost::mpi::comm_attach); \
+	if(env && world) { \
+		if(env->isComplete()) { \
+			unsigned int unit_rank = env->get_rank(_unit_type); \
+			boost::mpi::broadcast(world, value, unit_rank); \
+			std::cout << "Broadcast executed " << " root: "<< _unit_type << " - " << unit_rank << std::endl; \
+		} \
+	} \
+}
+#define Basic_Communicator_broadcast_array_impl(datatype) \
+void Basic_Communicator::broadcast(const Unit_Type& _unit_type, datatype* values, int n) { \
+	boost::mpi::communicator world(comm, boost::mpi::comm_attach); \
+	if(env && world) { \
+		if(env->isComplete()) { \
+			unsigned int unit_rank = env->get_rank(_unit_type); \
+			boost::mpi::broadcast(world, values, n, unit_rank); \
+			std::cout << "Broadcast executed " << " root: "<< _unit_type << " - " << unit_rank << std::endl; \
+		} \
+	} \
+}
+
+#define Basic_Communicator_gather_impl(datatype) \
+void Basic_Communicator::gather(const Unit_Type& _unit_type, const datatype& in_value, datatype* out_values) { \
+	boost::mpi::communicator world(comm, boost::mpi::comm_attach); \
+	if(env && world) { \
+		if(env->isComplete()) { \
+			unsigned int unit_rank = env->get_rank(_unit_type); \
+			boost::mpi::gather(world, in_value, out_values, unit_rank); \
+			std::cout << "Gather executed " << " root: "<< _unit_type << " - " << unit_rank << std::endl; \
+		} \
+	} \
+}
 namespace Pobcpp {
 
 Basic_Communicator::Basic_Communicator() : env(0)  { }
@@ -17,74 +78,30 @@ void Basic_Communicator::set_intracomm(MPI_Comm _comm) {
 	comm = _comm;
 }
 
-void Basic_Communicator::send(const Unit_Type& _unit_type, const int& _data, const int _tag) {
-	boost::mpi::communicator world(comm, boost::mpi::comm_attach);
-	if(env && world) {
-		if(env->isComplete()) {
-			unsigned int unit_rank = env->get_rank(_unit_type);
-			world.send(unit_rank, _tag, _data);	
-			std::cout << "Sending " << _data << " to "<< _unit_type << " - " << unit_rank << std::endl;
-		}
-	}
-}
+Basic_Communicator_send_impl(int);
+Basic_Communicator_send_impl(float);
+Basic_Communicator_send_impl(double);
+Basic_Communicator_send_impl(std::string);
 
-void Basic_Communicator::send(const Unit_Type& _unit_type, const std::string& _data, const int _tag) {
-	boost::mpi::communicator world(comm, boost::mpi::comm_attach);
-	if(env) {
-		if(env->isComplete()) {
-			unsigned int unit_rank = env->get_rank(_unit_type);
-			world.send(unit_rank, _tag, _data);	
-			std::cout << "Sending " << _data << " to "<< _unit_type << " - " << unit_rank << std::endl;
-		}
-	}
-}
+Basic_Communicator_receive_impl(int);
+Basic_Communicator_receive_impl(float);
+Basic_Communicator_receive_impl(double);
+Basic_Communicator_receive_impl(std::string);
 
-int Basic_Communicator::receive(const Unit_Type& _unit_type, const int& _data_type, const int _tag) {
-	boost::mpi::communicator world(comm, boost::mpi::comm_attach);
-	int data;
-	if(env && world) {
-		if(env->isComplete()) {
-			unsigned int unit_rank = env->get_rank(_unit_type);
-			world.recv(unit_rank , _tag, data);
-			std::cout << "Received " << data << " from "<< _unit_type << " - " << unit_rank << std::endl;
-		}
-	}
-	return data;
-}
+Basic_Communicator_broadcast_impl(int);
+Basic_Communicator_broadcast_impl(float);
+Basic_Communicator_broadcast_impl(double);
+Basic_Communicator_broadcast_impl(std::string);
 
-std::string Basic_Communicator::receive(const Unit_Type& _unit_type, const std::string& _data_type, const int _tag) {
-	boost::mpi::communicator world(comm, boost::mpi::comm_attach);
-	std::string data;
-	if(env) {
-		if(env->isComplete()) {
-			unsigned int unit_rank = env->get_rank(_unit_type);
-			world.recv(unit_rank , _tag, data);
-			std::cout << "Received " << data << " from "<< _unit_type << " - " << unit_rank << std::endl;
-		}
-	}
-	return data;
-}
+Basic_Communicator_broadcast_array_impl(int);
+Basic_Communicator_broadcast_array_impl(float);
+Basic_Communicator_broadcast_array_impl(double);
+Basic_Communicator_broadcast_array_impl(std::string);
 
-void Basic_Communicator::broadcast(const Unit_Type& _unit_type, int& value, unsigned int _enumi) {
-	boost::mpi::communicator world(comm, boost::mpi::comm_attach);
-	if(env && world) {
-		if(env->isComplete()) {
-			unsigned int unit_rank = env->get_rank(_unit_type);
-			boost::mpi::broadcast(world, value, unit_rank);
-			std::cout << "Broadcast executed " << " root: "<< _unit_type << " - " << unit_rank << std::endl;
-		}
-	}
-}
-void Basic_Communicator::broadcast(const Unit_Type& _unit_type, std::string& value, unsigned int _enumi) {
-	boost::mpi::communicator world(comm, boost::mpi::comm_attach);
-	if(env && world) {
-		if(env->isComplete()) {
-			unsigned int unit_rank = env->get_rank(_unit_type);
-			boost::mpi::broadcast(world, value, unit_rank);
-			std::cout << "Broadcast executed " << " root: "<< _unit_type << " - " << unit_rank << std::endl;
-		}
-	}
-}
+Basic_Communicator_gather_impl(int);
+Basic_Communicator_gather_impl(float);
+Basic_Communicator_gather_impl(double);
+Basic_Communicator_gather_impl(std::string);
 }
 
 
